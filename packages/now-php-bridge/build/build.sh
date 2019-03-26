d55907b5bdd9ebb16cb67d7275a7be4b1af30c63
johan <johan@baboons.se>
2019-03-26 15:56:55 +0100

    Changes to PHP 7.3 Improves docker builder

--- packages/now-php-bridge/build/build.sh
+++ packages/now-php-bridge/build/build.sh
@@ -5,13 +5,18 @@ docker rmi now-php-docker-image --force
 docker build . -t now-php-docker-image
 docker run now-php-docker-image
 docker run now-php-docker-image /bin/cat /usr/sbin/php-fpm > ../native/php-fpm
+docker run now-php-docker-image /bin/cat /usr/bin/php > ../native/php
 docker run now-php-docker-image /bin/cat /root/app/php.ini > ../native/php.ini
 docker run now-php-docker-image /bin/cat /root/app/php-fpm.ini > ../native/php-fpm.ini
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/curl.so > ../native/modules/curl.so
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/json.so > ../native/modules/json.so
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/mbstring.so > ../native/modules/mbstring.so
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/mysqli.so > ../native/modules/mysqli.so
-docker run now-php-docker-image /bin/cat /usr/lib64/mysql/libmysqlclient.so.16 > ../native/modules/libmysqlclient.so.16
+docker run now-php-docker-image /bin/cat /usr/lib64/libmcrypt.so.4 > ../native/modules/libmcrypt.so.4
 docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/opcache.so > ../native/modules/opcache.so
-docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/phar.so > ../native/modules/phar.so
+docker run now-php-docker-image /bin/cat /usr/lib64/libargon2.so.0 > ../native/libargon2.so.0
+
+while read module; do
+  docker run now-php-docker-image /bin/cat /usr/lib64/php/modules/$module.so > ../native/modules/$module.so
+done <php-modules.conf
+
+sed -i'.original' 's#/root/app/modules/#/root/app/native/modules/#g' ../native/php.ini
+
 chmod +x ../native/php-fpm
+chmod +x ../native/php
--- packages/now-php-bridge/build/test.php
+++ packages/now-php-bridge/build/test.php
@@ -1,4 +1,11 @@
 <?php
-mysqli_connect();
+if (!extension_loaded('mysqli')) {
+    die("Mysqli extension not loaded!" . PHP_EOL);
+}
+
+if (!class_exists("\\Phalcon\Version")) {
+    die("Phalcon extension not loaded!" . PHP_EOL);
+}
+
 print('php_sapi_name=' . php_sapi_name() . PHP_EOL);
 print('opcache_enabled=' . opcache_get_status()['opcache_enabled'] . PHP_EOL);
